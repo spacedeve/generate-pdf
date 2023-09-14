205daf3930dc999f35a886308a8dfa0e6fba4006
spaceman <oabdusamatov@gmail.com>
2023-09-15 00:27:43 +0500

    Modified script.js

--- public/script.js
+++ public/script.js
@@ -12,15 +12,44 @@ document.getElementById('userInfoForm').addEventListener('submit', e => {
     const cityStateZipCode = document.getElementById('city-state-zip-code').value;
     const accountNumbers = document.getElementById('account-numbers').value;
 
+    const images = [
+        'fw9_page-0001.jpg',
+        'fw9_page-0002.jpg',
+        'fw9_page-0003.jpg',
+        'fw9_page-0004.jpg',
+        'fw9_page-0005.jpg',
+        'fw9_page-0006.jpg'
+    ];
+
+    generatePDF(images);
+});
+
+fucntion generatePDF(images) {
 
     const doc = new jsPDF();
+    images.forEach((imageName, index) => {
+        if (index > 0) {
+            doc.addPage();
+        }
+    });
+
     const img = new Image();
-    img.src = 'bg-image/fw9_page-0001.jpg';
+    img.src = `bg-image/${imageName}`;
 
     img.onload = () => {
         doc.addImage(img, 'JPEG', 0, 0, 210, 297);
         doc.setFontSize(10);
         doc.text(name, 25, 40);
-        doc.save('output.pdf');
+        doc.text(`Selected Tax: ${selectedTax}`, 15, 35);
+        doc.text(`Tax Classification: ${taxClassification}`, 15, 45);
+        doc.text(`Other Federal Tax Classification: ${otherFederalTaxClassification}`, 15, 55);
+        doc.text(`Exempt Payee Code: ${exemptPayeeCode}`, 15, 65);
+        doc.text(`Exemption from FATCA Reporting Code: ${exemptionFromFatcaReportingCode}`, 15, 75);
+        doc.text(`Address: ${address}`, 15, 85);
+        doc.text(`City, State, Zip Code: ${cityStateZipCode}`, 15, 95);
+        doc.text(`Account Numbers: ${accountNumbers}`, 15, 105);
     };
-});
+
+
+    doc.save('output.pdf');
+}
